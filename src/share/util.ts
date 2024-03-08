9b14ac698b34b883118ad46d2c77349f72e703f1
Qing Miao <digisanqm@outlook.com>
2024-03-08 14:41:39 +1100

    * auto download report if submission validation failed

--- src/share/util.ts
+++ src/share/util.ts
@@ -152,3 +152,21 @@ export const oriName = (name: string) => {
     }
     return name
 }
+
+export const isUrl = (url: string) => {
+    try {
+        new URL(url);
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
+export const download_file = (url: string, file_name: string) => {
+    const anchor = document.createElement("a");
+    anchor.href = url;
+    anchor.download = file_name;
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+}
